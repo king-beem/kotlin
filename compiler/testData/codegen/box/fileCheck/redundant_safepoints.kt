180a1b12dc4e699263e6b79fd07a2560708fda03
Vladimir Sukharev <vladimir.sukharev@jetbrains.com>
2023-10-27 22:51:09 +0200

    [K/N] Migrate filecheck test `redundant_safepoints` to new infra
    ^KT-62157

---
+++ compiler/testData/codegen/box/fileCheck/redundant_safepoints.kt
@@ -0,0 +1,68 @@
+// TARGET_BACKEND: NATIVE
+// FILECHECK_STAGE: RemoveRedundantSafepoints
+
+// This test checks:
+// - there is only one safepoint per function
+// - safepoint function is inlined in OPT mode, unless SMALLBINARY is needed (for ex, watchos_arm32)
+// Might fail under -Xbinary=gc=stwms and -Xbinary=gc=noop. In this case, just add ignore clause.
+
+@file:Suppress("INVISIBLE_REFERENCE", "INVISIBLE_MEMBER")
+import kotlin.native.Retain
+
+class C
+
+fun f(): Any {
+    return C()
+}
+
+fun g() = f()
+
+// CHECK-LABEL: define %struct.ObjHeader* @"kfun:#h(kotlin.Boolean){}kotlin.Any"
+@Retain
+fun h(cond: Boolean): Any {
+    // CHECK-SMALLBINARY: {{call .*Kotlin_mm_safePointFunctionPrologue}}
+    // CHECK-BIGBINARY-DEBUG: {{call .*Kotlin_mm_safePointFunctionPrologue}}
+    // CHECK-BIGBINARY-OPT: _ZN12_GLOBAL__N_115safePointActionE
+
+    // CHECK-SMALLBINARY-NOT: _ZN12_GLOBAL__N_115safePointActionE
+    // CHECK-BIGBINARY-DEBUG-NOT: _ZN12_GLOBAL__N_115safePointActionE
+    // CHECK-BIGBINARY-OPT-NOT: {{call .*Kotlin_mm_safePointFunctionPrologue}}
+
+    // CHECK-SMALLBINARY-NOT: {{call .*Kotlin_mm_safePointFunctionPrologue}}
+    // CHECK-BIGBINARY-DEBUG-NOT: {{call .*Kotlin_mm_safePointFunctionPrologue}}
+    // CHECK-BIGBINARY-OPT-NOT: _ZN12_GLOBAL__N_115safePointActionE
+    // CHECK: br
+    if (cond) {
+        // CHECK-SMALLBINARY-NOT: {{call .*Kotlin_mm_safePointFunctionPrologue}}
+        // CHECK-BIGBINARY-DEBUG-NOT: {{call .*Kotlin_mm_safePointFunctionPrologue}}
+        // CHECK-BIGBINARY-OPT-NOT: _ZN12_GLOBAL__N_115safePointActionE
+        // CHECK: br
+        return listOf(C(), C())
+    } else {
+        // CHECK-SMALLBINARY-NOT: {{call .*Kotlin_mm_safePointFunctionPrologue}}
+        // CHECK-BIGBINARY-DEBUG-NOT: {{call .*Kotlin_mm_safePointFunctionPrologue}}
+        // CHECK-BIGBINARY-OPT-NOT: _ZN12_GLOBAL__N_115safePointActionE
+        return listOf(C(), C(), C())
+    }
+// CHECK-LABEL: ret
+}
+
+// CHECK-LABEL: define %struct.ObjHeader* @"kfun:#box(){}kotlin.String"
+@Retain
+fun box(): String {
+    // CHECK-SMALLBINARY: {{call .*Kotlin_mm_safePointFunctionPrologue}}
+    // CHECK-BIGBINARY-DEBUG: {{call .*Kotlin_mm_safePointFunctionPrologue}}
+    // CHECK-BIGBINARY-OPT: _ZN12_GLOBAL__N_115safePointActionE
+
+    // CHECK-SMALLBINARY-NOT: _ZN12_GLOBAL__N_115safePointActionE
+    // CHECK-BIGBINARY-DEBUG-NOT: _ZN12_GLOBAL__N_115safePointActionE
+    // CHECK-BIGBINARY-OPT-NOT: {{call .*Kotlin_mm_safePointFunctionPrologue}}
+
+    // CHECK-SMALLBINARY-NOT: {{call .*Kotlin_mm_safePointFunctionPrologue}}
+    // CHECK-BIGBINARY-DEBUG-NOT: {{call .*Kotlin_mm_safePointFunctionPrologue}}
+    // CHECK-BIGBINARY-OPT-NOT: _ZN12_GLOBAL__N_115safePointActionE
+    println(g())
+    println(h(true))
+    return "OK"
+// CHECK-LABEL: ret
+}
